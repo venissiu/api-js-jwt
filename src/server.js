7049c5ffebbf808e88e1ad30b75616a6b67407d9
NiiVx <vinicios241@gmail.com>
2022-10-29 01:54:07 -0300

    fix: refactor to use now the separated routes

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = require('./api');
 const routes = require('./routes');
 const validateJWT = require('./middlewares/auth/validateJWT');
-const userRoutes = require('./routes/userRouter')
+const userRoutes = require('./routes/user');
+const postRouter = require('./routes/post');
+
 const apiRoutes = express.Router();
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -12,13 +14,10 @@ app.use((err, req, res, _next) => res.status(500).json({ message: 'Erro no servi
 apiRoutes.post('/login', routes.login);
 apiRoutes.get('/categories', validateJWT, routes.categories.findAll);
 apiRoutes.post('/categories', validateJWT, routes.categories.create);
-apiRoutes.get('/post', validateJWT, routes.post.findAll);
-apiRoutes.get('/post/:id', validateJWT, routes.post.findById);
-apiRoutes.post('/post', validateJWT, routes.post.create);
-apiRoutes.delete('/post/:id', validateJWT, routes.post.delete);
-apiRoutes.put('/post/:id', validateJWT, routes.post.update);
+
 app.use(apiRoutes);
-app.use(userRoutes)
+app.use(postRouter);
+app.use(userRoutes);
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
