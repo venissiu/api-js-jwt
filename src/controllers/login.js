ddd1fc7c9214f3f2c22e091cbb6f28e6c8aba3c4
NiiVx <vinicios241@gmail.com>
2022-10-29 01:50:58 -0300

    feat: use userService to find by email instead calling the user model

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,32 +1,39 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const { User } = require('../database/models');
-const rescue = require('../rescue');
+const userService = require('../services/userService');
 
-const SECRET = process.env.JWT_SECRET;
-
+const secret = process.env.JWT_SECRET;
 const validateBody = (body, res) => {
   const { email, password } = body;
 
   if (!email || !password) {
-    res.status(400).json({ message: 'Some required fields are missing' });
+    res
+      .status(400)
+      .json({ message: 'Some required fields are missing' });
     return false;
   }
 
   return true;
 };
 
-module.exports = rescue(async (req, res) => {
-  const { email, password } = req.body;
+module.exports = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  if (!validateBody(req.body, res)) return;
+    if (!validateBody(req.body, res)) return;
 
-  const user = await User.findOne({ where: { email } });
-  
-  if (!user || !bcrypt.compareSync(password, user.password)) {
-    return res.status(400).json({ message: 'Invalid fields' });
+    const user = await userService.findByEmail(email);
+
+    if (!user || user.password !== password) {
+      return res.status(400)
+        .json({ message: 'Invalid fields' });
+    }
+    const jwtConfig = { expiresIn: '1d', algorithm: 'HS256',
+    };
+    const token = jwt.sign({ data: user }, secret, jwtConfig);
+    return res.status(200).json({ token });
+     } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'Erro interno', error: err.message });
   }
-  const jwtConfig = { expiresIn: '1d', algorithm: 'HS256' };
-  const token = jwt.sign({ data: user }, SECRET, jwtConfig);
-  return res.status(200).json({ token });
-});
+};
